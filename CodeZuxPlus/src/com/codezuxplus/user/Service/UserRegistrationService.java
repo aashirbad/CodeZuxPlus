6a43cd2297ae445a434008d427cac58b01833780
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2019-11-09 09:47:49 +0530

    Log in and Sign Up Updated

--- CodeZuxPlus/src/com/codezuxplus/user/Service/UserRegistrationService.java
+++ CodeZuxPlus/src/com/codezuxplus/user/Service/UserRegistrationService.java
@@ -6,19 +6,18 @@ import com.codezuxplus.user.Bean.UserRegisterBean;
 import com.codezuxplus.user.DAO.UserRegisterDAO;
 
 public class UserRegistrationService {
-	
+
 	UserRegisterDAO UserDao = new UserRegisterDAO();
+
 	
 	public void userRedgService(UserRegisterBean userBean) throws SQLException {
+
 		int flag;
-		
-		//flag = UserDao.registerUser(userBean);
-		//return flag;
-		
+
 	}
-	
+
 	public void show(UserRegisterBean userRedgBean) {
-		//UserDao.daoShow(userRedgBean);		
+		// UserDao.daoShow(userRedgBean);
 	}
 
 }
